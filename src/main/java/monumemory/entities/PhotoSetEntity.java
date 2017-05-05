188cc46730f2f7e1b5c586d90b4d83c6bf25dbaf
SingularityA <singularity.m101@gmail.com>
2017-05-06 01:28:13 +0300

    Add PhotoSet entity

---
+++ src/main/java/monumemory/entities/PhotoSetEntity.java
@@ -0,0 +1,16 @@
+package monumemory.entities;
+
+import lombok.Data;
+
+/**
+ * @author SingularityA
+ */
+@Data
+public class PhotoSetEntity {
+
+    private Integer id;
+
+    private String description;
+
+    private Integer monumentId;
+}
