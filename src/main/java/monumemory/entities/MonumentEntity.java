197ad5b9f4976b91ec7243ed6b1bd0ec5e39f83b
SingularityA <singularity.m101@gmail.com>
2017-05-06 01:27:44 +0300

    Add Monument entity

---
+++ src/main/java/monumemory/entities/MonumentEntity.java
@@ -0,0 +1,18 @@
+package monumemory.entities;
+
+import lombok.Data;
+
+/**
+ * @author SingularityA
+ */
+@Data
+public class MonumentEntity {
+
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private String history;
+}
