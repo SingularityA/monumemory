71e05a42657023aac7544123b16553b1fe20f732
SingularityA <singularity.m101@gmail.com>
2017-05-05 17:54:45 +0300

    Add application class

---
+++ src/main/java/monumemory/MonuMemoryApplication.java
@@ -0,0 +1,15 @@
+package monumemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SungularityA
+ */
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+@SpringBootApplication
+public class MonuMemoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonuMemoryApplication.class, args);
+    }
+}
