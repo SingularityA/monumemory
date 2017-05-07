e0a77f1ced64be8575179a935bc1a2ae89f5f055
SingularityA <singularity.m101@gmail.com>
2017-05-07 05:22:48 +0300

    Add Monument model empty

---
+++ src/main/java/monumemory/models/empty/MonumentModelEmpty.java
@@ -0,0 +1,50 @@
+package monumemory.models.empty;
+
+import java.util.List;
+import monumemory.models.MonumentModelInterface;
+import monumemory.models.PhotoSetModelInterface;
+import monumemory.models.exceptions.CallMethodOnEmptyModelException;
+import monumemory.models.exceptions.CallMethodOnEmptyModelsListException;
+
+public class MonumentModelEmpty extends AbstractModelEmpty implements MonumentModelInterface {
+
+    MonumentModelEmpty(Integer id) {
+        super(id);
+    }
+
+    public void setId(Integer id) {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    public String getName() {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    public void setName(String name) {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    public String getDescription() {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    public void setDescription(String description) {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    public String getHistory() {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    public void setHistory(String history) {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    public List<PhotoSetModelInterface> getPhotoSets() {
+        throw new CallMethodOnEmptyModelsListException();
+    }
+
+    public void setPhotoSets(List<PhotoSetModelInterface> photoSets) {
+        throw new CallMethodOnEmptyModelsListException();
+    }
+}
