af3b06ad334825f3e793bce4789d57f44dd297a7
SingularityA <singularity.m101@gmail.com>
2017-05-07 03:23:50 +0300

    Little fixes in basic tables migration

--- src/main/resources/migrations/scripts/20170505215141_create_basic_tables.sql
+++ src/main/resources/migrations/scripts/20170505215141_create_basic_tables.sql
@@ -1,30 +1,30 @@
 -- // create_basic_tables
 -- Migration SQL that makes the change goes here.
 CREATE TABLE monuments (
-  id          SERIAL        NOT NULL,
-  name        VARCHAR(100)  NOT NULL,
-  description VARCHAR(250)  NOT NULL,
-  history     VARCHAR(500)  NOT NULL,
+  id          SERIAL       NOT NULL,
+  name        VARCHAR(100) NOT NULL,
+  description VARCHAR(250) NOT NULL,
+  history     VARCHAR(500) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE photo_set (
-  id          SERIAL  NOT NULL,
+CREATE TABLE photo_sets (
+  id          SERIAL        NOT NULL,
   description VARCHAR(250),
-  monument_id INTEGER NOT NULL,
+  monument_id INTEGER       NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE photos (
-  id           SERIAL       NOT NULL,
+  id           SERIAL        NOT NULL,
   name         VARCHAR(100),
-  path         VARCHAR(250) NOT NULL,
-  photo_set_id INTEGER      NOT NULL,
+  path         VARCHAR(250)  NOT NULL,
+  photo_set_id INTEGER       NOT NULL,
   PRIMARY KEY (id)
 );
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 DROP TABLE monuments;
-DROP TABLE photo_set;
+DROP TABLE photo_sets;
 DROP TABLE photos;
