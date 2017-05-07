c9f274b8b0c8e9374523f92830b49446ce32075a
SingularityA <singularity.m101@gmail.com>
2017-05-07 03:25:15 +0300

    Create PhotoSet DAO

---
+++ src/main/java/monumemory/dao/PhotoSetDao.java
@@ -0,0 +1,35 @@
+package monumemory.dao;
+
+import java.util.List;
+import monumemory.entities.PhotoSetEntity;
+import org.apache.ibatis.annotations.*;
+
+/**
+ * @author SingularityA
+ */
+@Mapper
+public interface PhotoSetDao {
+    @Results(
+            id = "PhotoSet",
+            value = {
+                    @Result(property = "id", column = "id"),
+                    @Result(property = "description", column = "description"),
+                    @Result(property = "monumentId", column = "monument_id")
+            })
+    @Select("SELECT * FROM photo_sets")
+    List<PhotoSetEntity> findAll();
+
+    @Select("SELECT * FROM photo_sets WHERE id=#{id}")
+    @ResultMap("PhotoSet")
+    PhotoSetEntity find(Integer id);
+
+    @Insert("INSERT INTO photo_sets (description, monument_id) values (#{description}, #{monumentId})")
+    @Options(useGeneratedKeys = true, keyColumn = "id")
+    void insert(PhotoSetEntity photoSetEntity);
+
+    @Update("UPDATE photo_sets SET description=#{description}, monument_id=#{monumentId} WHERE id=#{id}")
+    void update(PhotoSetEntity photoSetEntity);
+
+    @Delete("DELETE FROM photo_sets WHERE id=#{id}")
+    void delete(PhotoSetEntity photoSetEntity);
+}
